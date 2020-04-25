ccedc35b5dd43c569754a71fb8e32390ae3d34ff
joaoofreitas <joaoofreitas00@gmail.com>
2020-04-26 00:00:51 +0100

    Updates

---
+++ precache-manifest.4b469737baaf4a62c9979f90330664f4.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b35deed2998c14f07e7b2c058e2ffcc",
+    "url": "/react-personal-website/index.html"
+  },
+  {
+    "revision": "503ec7c0d3b044d2ebd5",
+    "url": "/react-personal-website/static/css/2.1ce9b504.chunk.css"
+  },
+  {
+    "revision": "d5393014fd46cfa80f02",
+    "url": "/react-personal-website/static/css/main.d0e2bbf8.chunk.css"
+  },
+  {
+    "revision": "503ec7c0d3b044d2ebd5",
+    "url": "/react-personal-website/static/js/2.f74179a5.chunk.js"
+  },
+  {
+    "revision": "e18637604789af6a40503615de5bd745",
+    "url": "/react-personal-website/static/js/2.f74179a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5393014fd46cfa80f02",
+    "url": "/react-personal-website/static/js/main.774d4ac3.chunk.js"
+  },
+  {
+    "revision": "69bbc74fcaff4877515f",
+    "url": "/react-personal-website/static/js/runtime-main.4ec72326.js"
+  },
+  {
+    "revision": "2773f21357b11d0159f2075b88b2122c",
+    "url": "/react-personal-website/static/media/arduino.2773f213.svg"
+  },
+  {
+    "revision": "18713802ffbb3902aded25597dc4fcb7",
+    "url": "/react-personal-website/static/media/cpp.18713802.svg"
+  },
+  {
+    "revision": "2e0fcd7b793e8353f3e272dc38492020",
+    "url": "/react-personal-website/static/media/docker.2e0fcd7b.svg"
+  },
+  {
+    "revision": "05f1cdadfe476395f60e233b15c22155",
+    "url": "/react-personal-website/static/media/icons-16.05f1cdad.eot"
+  },
+  {
+    "revision": "3c1c220e7a18286503fb431c7a7fe183",
+    "url": "/react-personal-website/static/media/icons-16.3c1c220e.woff"
+  },
+  {
+    "revision": "3cde8748332d1de6b1ae1c2dc5850754",
+    "url": "/react-personal-website/static/media/icons-16.3cde8748.ttf"
+  },
+  {
+    "revision": "0a5c76518a68c185baa2c6744456918c",
+    "url": "/react-personal-website/static/media/icons-20.0a5c7651.eot"
+  },
+  {
+    "revision": "51ec31f302d0072808e1f83f85fea4cd",
+    "url": "/react-personal-website/static/media/icons-20.51ec31f3.ttf"
+  },
+  {
+    "revision": "cef8cdbb9d0ba82e6e19fb0eeba2ac3d",
+    "url": "/react-personal-website/static/media/icons-20.cef8cdbb.woff"
+  },
+  {
+    "revision": "aa7fd4969cba0bafaa8ee1e206f246b0",
+    "url": "/react-personal-website/static/media/linux.aa7fd496.svg"
+  },
+  {
+    "revision": "f4a010b8cbaa9799755beabc06b0f910",
+    "url": "/react-personal-website/static/media/node.f4a010b8.svg"
+  },
+  {
+    "revision": "3b2095cf82d645d7d41b838a921b2b03",
+    "url": "/react-personal-website/static/media/profilePic.3b2095cf.jpeg"
+  },
+  {
+    "revision": "fbef321e03c16bcae42560f8507757d6",
+    "url": "/react-personal-website/static/media/python.fbef321e.svg"
+  }
+]);
